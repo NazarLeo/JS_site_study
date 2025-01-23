71524bc694e175e56bdf23c0e85c69de0e93103b
Nazar <nazarleochko2006@gmail.com>
2025-01-23 17:15:29 +0200

    Array Tasks

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+// Место для первой задачи
+function firstTask() {
+  for( let i = 5; i<=10; i++){
+    console.log(i);
+  }
+  
+  
+}
 
-personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// Место для второй задачи
+function secondTask() {
+  for( let i = 20; i === 10; i--){
+    if (i === 13) break;
+    console.log(i);
+  }
+  
+  
+}
 
-const a = prompt('Один из последних фильмов?', ''),
-      b = prompt('На сколькько оцените?', ''),
-      c = prompt('Один из последних фильмов?', ''),
-      d = prompt('На сколькько оцените?', '');
+// Место для третьей задачи
+function thirdTask() {
+  for( let i = 1; i<=10; i++){
+    if ( !(i % 2) )   console.log(i);
+  }  
+  
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// Место для четвертой задачи
+function fourthTask() {
+  
+  let i =2;
 
-console.log(personalMovieDB);
+  while (i<=16) {
+    if (i % 2 === 0) {
+      i++;
+      continue;
+    } else {
+      console.log(i);
+      i++;
+    }
+  }  
+  
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  for( let i = 5; i <= 10; i++){
+    arrayOfNumbers[i] = i;
+  }
+  
+  return arrayOfNumbers;
+}
