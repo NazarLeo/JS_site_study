54924deaa7789848b71c4c9f428b905b0d14ef73
Nazar <nazarleochko2006@gmail.com>
2025-02-21 14:39:40 +0200

    db

--- js/script.js
+++ js/script.js
@@ -88,8 +88,7 @@ window.addEventListener( 'DOMContentLoaded', ()=>{
   // Modal
 
   const modalTriger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector ('[data-close]');
+        modal = document.querySelector('.modal');
   
   openModal = () => {
     modal.classList.add('show');
@@ -104,24 +103,21 @@ window.addEventListener( 'DOMContentLoaded', ()=>{
     })
   });
 
-
   closeModal = () =>{
     modal.classList.add('hide');
     modal.classList.remove('show');
     document.body.style.overflow = '';
   };
-
-  modalCloseBtn.addEventListener('click', closeModal);
   
   modal.addEventListener('click', (e) => {
-    if (e.target === modal) closeModal();
+    if (e.target === modal || e.target.getAttribute('data-close') == '') closeModal();
   });
   
   document.addEventListener('keydown', (e) => {
     if (e.code === 'Escape' && modal.classList.contains('show')) closeModal();
   })
 
-  // autoOpen = setTimeout(openModal, 3000);
+  autoOpen = setTimeout(openModal, 50000); 
 
   showModalByScroll = () => {
     if(window.pageYOffset + document.documentElement.clientHeight >=
@@ -170,81 +166,100 @@ window.addEventListener( 'DOMContentLoaded', ()=>{
 
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    18,
-    ".menu .container"
-  ).render();
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    16,
-    ".menu .container"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if(!res.ok) throw new Error(`Could not fetch ${url}, status ${res.status}`)
+
+    return await res.json();
+  }
+
+  getResource('http://localhost:3000/menu')
+    .then(data => {
+      data.forEach(({img, altimg, title, descr, price})=>{
+        new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+      });
+    })
 
   const forms = document.querySelectorAll('form');
 
   const message = {
-    loading: 'Загрузка',
+    loading: 'img/form/spinner.svg',
     succes: 'Спасибо! Скоро мы с вами свяжемся',
     failure: 'Что-то пошло не так...'
   };
 
   forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
   })
 
-  function postData (form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers:{
+        'Content-type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  }
+
+  function bindPostData (form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      const statusMessage = document.createElement('div');
-      statusMessage.classList.add('status');
-      statusMessage.textContent = message.loading;
-      form.append(statusMessage);
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `
+      form.insertAdjacentElement('afterend', statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-
-      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function(value, key) {
-        object[key] = value;
+      const json = JSON.stringify(Object.fromEntries(formData.entries()))
+
+      postData('http://localhost:3000/requests', json)
+      .then(data=>{
+        console.log(data);
+        showThanksModal(message.succes);
+        statusMessage.remove();
+      }).catch(()=>{
+        showThanksModal(message.failure);
+      }).finally(()=>{
+        form.reset();
       })
-
-      const json = JSON.stringify(object);
-
-      request.send(json);
-
-      request.addEventListener('load', ()=>{
-        if (request.status === 200){
-          console.log(request.response);
-          statusMessage.textContent = message.succes;
-          form.reset();
-          setTimeout(()=>{
-            statusMessage.remove();
-          }, 2000);
-        } else {
-          statusMessage.textContent = message.failure;
-        }
-      })
-
     })
   }
+
+  function showThanksModal(message){
+    const prevModalDialog = document.querySelector('.modal__dialog')
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class = "modal__content">
+        <div class = "modal__close" data-close>×</div>
+        <div class = "modal__title">${message}</div>
+      </div>
+    `
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.remove('hide');
+      prevModalDialog.classList.add('show');
+      closeModal();
+    }, 4000)
+  }
+
+  fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 
 })
